4ed3d8e61dd35b17bd3dd3da8ff1ec3e91e3fda8
gui <guilhermev2huehue@gmail.com>
2024-06-23 19:15:30 -0300

    refactor: refactoring handlersA

--- main.go
+++ main.go
@@ -6,10 +6,23 @@ import (
 	"time"
 )
 
-func main() {
-    mux := http.NewServeMux()
+type apiConfig struct {
+    fileserverHits int
+}
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    
+
+    return next
+}
+
+func main() {
     port := ":8080"
+    apiCfg := apiConfig{}
+   
+    mux := http.NewServeMux()
+    mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("./")))))
+    mux.HandleFunc("/healthz", handlerReadiness)
 
     s := http.Server{
 	    Addr:           port,
@@ -19,15 +32,12 @@ func main() {
 	    MaxHeaderBytes: 1 << 20,
     }
 
-    mux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
-    mux.Handle("/app/assets", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))))
-    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Add("Content-Type", "text/plain; charset=utf-8")    
-        w.WriteHeader(200)
-        w.Write([]byte("OK"))
-    })
-
     log.Printf("🚀 Server started at: http://localhost%s\n", port)
     log.Fatal(s.ListenAndServe())
 }
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Content-Type", "text/plain; charset=utf-8")    
+    w.WriteHeader(200)
+    w.Write([]byte("OK"))
+}
