c1445a549470d9f9b27d602a4df4988c72b457fb
gui <guilhermev2huehue@gmail.com>
2024-06-23 18:48:54 -0300

    feat: adding custom handler and health route

--- main.go
+++ main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
     mux := http.NewServeMux()
+
+    port := ":8080"
+
     s := http.Server{
-	    Addr:           ":8080",
+	    Addr:           port,
 	    Handler:        mux,
 	    ReadTimeout:    10 * time.Second,
 	    WriteTimeout:   10 * time.Second,
 	    MaxHeaderBytes: 1 << 20,
     }
 
-    mux.Handle("/", http.FileServer(http.Dir("./")))
+    mux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
+    mux.Handle("/app/assets", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))))
+    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Add("Content-Type", "text/plain; charset=utf-8")    
+        w.WriteHeader(200)
+        w.Write([]byte("OK"))
+    })
 
-    s.ListenAndServe()
+    log.Printf("🚀 Server started at: http://localhost%s\n", port)
+    log.Fatal(s.ListenAndServe())
 }
 
